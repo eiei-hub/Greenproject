eaf194fe904904b34ba4c1a3c63242100aeedb55
eiei-hub <1572649053@qq.com>
2025-04-11 10:22:34 +0800

    new

--- green.java
+++ green.java
@@ -2,6 +2,7 @@ public class green {
     public static void main(String[] args) {
         System.out.println("green");
         System.out.println("green1");
+        System.out.println("green2");
     }
     
 }
